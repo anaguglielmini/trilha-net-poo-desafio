9f600bb8f7212eed1c29d25b0b3b9ddacfcbdaff
Ana Luiza Migliavacca Guglielmini <ana.guglielmini@etec.sp.gov.br>
2024-10-30 10:45:07 -0300

    ToDo's implementados

--- Program.cs
+++ Program.cs
@@ -1,3 +1,14 @@
 ﻿using DesafioPOO.Models;
 
 // TODO: Realizar os testes com as classes Nokia e Iphone
+Console.WriteLine("Smartphone Nokia: ");
+Smartphone nokia = new Nokia("11987654123", "Modelo1", "123456789101213", 128);
+nokia.Ligar();
+nokia.ReceberLigacao();
+nokia.InstalarAplicativo("SoundCloud");
+
+Console.WriteLine("Smartphone Iphone: ");
+Smartphone iphone = new Iphone("11932165498", "Modelo2", "032165498703216", 64);
+iphone.Ligar();
+iphone.ReceberLigacao();
+iphone.InstalarAplicativo("Samsung Wearable");
